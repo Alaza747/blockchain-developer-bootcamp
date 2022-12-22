c95f0b2c33edc66d2bbc3a3f5254b6ec671a4868
Anton Laza <anton.laza.0116@gmail.com>
2022-12-22 13:53:02 +0100

    configured hardhat.config to use localhost node

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
+// For more information see: https://hardhat.org/config/
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {}
+  }
 };
