3f0a326b3eb501c4f12b0bb4af371d263bb15889
Anton Laza <anton.laza.0116@gmail.com>
2022-12-22 13:45:52 +0100

    created Token.sol

---
+++ contracts/Token.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = "My Token"; // Declaring a public (visible to everyone on the blockchain) variable (belongs to the Contracts (e.g. not limitied to function's scope) Scope) variable
+
+}
