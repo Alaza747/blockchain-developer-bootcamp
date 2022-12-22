bbddcca4865af889bf826ed2a752ad93a5eb2a23
Anton Laza <anton.laza.0116@gmail.com>
2022-12-22 13:48:51 +0100

    deleted Greeter.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
+
+import "hardhat/console.sol"; // Enables console.log functionality
 
 contract Token {
     string public name = "My Token"; // Declaring a public (visible to everyone on the blockchain) variable (belongs to the Contracts (e.g. not limitied to function's scope) Scope) variable
