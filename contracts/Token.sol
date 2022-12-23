000afa061de9fa476123c6cc92c67f11d8834834
AntonL <anton.laza.0116@gmail.com>
2022-12-23 10:28:38 +0100

    Symbol Test implented

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ import "hardhat/console.sol"; // Enables console.log functionality
 
 contract Token {
     string public name = "TonyCoin"; // Declaring a public (visible to everyone on the blockchain) variable (belongs to the Contracts (e.g. not limitied to function's scope) Scope) variable
-
-
+    string public symbol = "TONY";
+    
 }
  
--- test/Token.js
+++ test/Token.js
@@ -5,7 +5,7 @@ const { ethers } = require("hardhat");
 describe('Token Contract', () => {
     // Tests go inside here...
     it('has correct name', async () => {
-        // Fetch Token from Blockchain
+    // Fetch Token from Blockchain
         const Token = await ethers.getContractFactory("Token")
         let token = await Token.deploy()
         
@@ -15,4 +15,18 @@ describe('Token Contract', () => {
         // Check that the name is correct
         expect(name).to.equal("TonyCoin")
     })
+
+    it('has correct symbol', async () => {
+        // Fetch Token from Blockchain
+            const Token = await ethers.getContractFactory("Token")
+            let token = await Token.deploy()
+            
+            // Read token symbol
+            const symbol = await token.symbol()
+            
+            // Check that the symbol is correct
+            expect(symbol).to.equal("TONY")
+        })
+
+
 })
