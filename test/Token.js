3ce0df03b2ff334d67aa19a1dd9bdf165a0abd2a
AntonL <anton.laza.0116@gmail.com>
2022-12-23 10:21:20 +0100

    3. Created First Test

---
+++ test/Token.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+
+describe('Token Contract', () => {
+    // Tests go inside here...
+    it('has a name', async () => {
+        // Fetch Token from Blockchain
+        const Token = await ethers.getContractFactory("Token")
+        let token = await Token.deploy()
+        
+        // Read token name
+        const name = await token.name()
+        
+        // Check that the name is correct
+        expect(name).to.equal("My Token")
+    })
+})
