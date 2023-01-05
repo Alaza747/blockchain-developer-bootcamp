a4fb66cec9979500c7a019138c32bce55a6e72f9
Anton Laza <anton.laza.0116@gmail.com>
2023-01-05 14:09:01 +0100

    allowance, approve function and tests

--- test/Token.js
+++ test/Token.js
@@ -12,7 +12,8 @@ describe('Token Contract', () => {
     let token, 
         accounts,
         deployer,
-        receiver
+        receiver,
+        exchange
 
     beforeEach(async () => {
         // Code goes in here...
@@ -25,6 +26,7 @@ describe('Token Contract', () => {
         accounts = await ethers.getSigners();
         deployer = accounts[0];
         receiver = accounts[1];
+        exchange = accounts[2];
     })
     
     // "Block" of testing 
@@ -107,5 +109,35 @@ describe('Token Contract', () => {
             })
         })
     })
+
+    describe("Approving Tokens", () => {
+        let amount, transaction, result
+
+        beforeEach(async () =>{
+            amount = tokens(100)
+            transaction = await token.connect(deployer).approve(exchange.address, amount)
+            result = await transaction.wait()
+        })
+        describe("Success", () => {
+            it(" allocates an allowance for delegated token spending", async () => {
+                expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+            })
+
+            it(" emits an Approval event", async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal("Approval");
+
+                const args = event.args
+                expect(args.owner).to.equal(deployer.address);
+                expect(args.spender).to.equal(exchange.address);
+                expect(args.value).to.equal(amount);
+            })
+        })
+        
+        describe("Failure", () => {
+            it(" rejects invalid spenders", async () => {
+                await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+            })
+        })
+    })
 })
-  
