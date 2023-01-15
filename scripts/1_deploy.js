b50f318a89c24435c7d929167ae1c3f3dbbbae67
Anton Laza <anton.laza.0116@gmail.com>
2023-01-15 08:56:14 +0100

    updated the accounts and added mETH and mDAI

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -5,16 +5,43 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 
+const { ethers } = require("hardhat");
+
 async function main() {
-    // Fetch contract to deploy
-    const Token = await ethers.getContractFactory("Token");
+  console.log("Preparing Deployment...")
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
 
-    // Deploy contract
-    const token = await Token.deploy();
+  // Fetch Accounts
+  const accounts = await ethers.getSigners();
 
-    // Fetch the copy of the token that is deployed to blockchain
-    await token.deployed();
-    console.log(`Token deployed to: ${token.address}`);
+  console.log(
+    `Accounts fetched:
+    \n${accounts[0].address}
+    \n${accounts[1].address}
+    \n`)
+
+  // Deploy contract
+  const tony = await Token.deploy("TonyCoin", "TONY", 1000000);
+  // Fetch the copy of the TONY token that is deployed to blockchain
+  await tony.deployed();
+  console.log(`TONY Token deployed to: ${tony.address}`);
+
+  const mETH = await Token.deploy('MockEther', 'mETH', 1000000);
+  // Fetch the copy of the TONY token that is deployed to blockchain
+  await mETH.deployed();
+  console.log(`mETH deployed to: ${mETH.address}`);
+
+  const mDAI = await Token.deploy('mockDAI', 'mDAI', 1000000);
+  // Fetch the copy of the TONY token that is deployed to blockchain
+  await mDAI.deployed();
+  console.log(`mDAI deployed to: ${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange deployed to: ${exchange.address}`);
+
 }
 
 main().catch((error) => {
