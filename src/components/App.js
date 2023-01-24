fe96db25bb248fa7fa2a5ce86d70282da4e5d00b
Anton Laza <anton.laza.0116@gmail.com>
2023-01-24 23:20:21 +0100

    copied abis, connected to blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,32 @@
+import { useEffect } from 'react';
+import { method } from 'lodash';
+import { ethers } from 'ethers';
+import config from '../config.json';
+import TOKEN_ABI from '../abis/Token.json';
 import '../App.css';
 
+
 function App() {
+
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    console.log(`Account 0 address is:`, accounts[0])
+
+    // Connect Ethers to the blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const { chainId } = await provider.getNetwork(provider)
+    console.log(`ChainID is:`, chainId)
+
+    // Token Smart Contract
+    const token = new ethers.Contract(config[31337].Tony.address, TOKEN_ABI, provider)
+    console.log(`Token contract's address is:`, token.address)
+    const symbol = await token.symbol()
+    console.log(`Symbol is:`, symbol)
+  }
+    
+  useEffect(() => {
+    loadBlockchainData();
+  })
 
   return (
     <div>
@@ -35,5 +61,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
