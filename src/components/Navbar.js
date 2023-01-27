5676b1dd3eac2c9e0f3ad40fe5e79988c9393a92
Anton Laza <anton.laza.0116@gmail.com>
2023-01-27 23:14:44 +0100

    created Navbar.js and started with the grid

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return(
+        <div className="exchange__header grid">
+            <div className="exchange__header--brand flex">
+                <h1>Tony Token Exchange</h1>
+            </div>
+
+            <div className="exchange__header--networks flex">
+
+            </div>
+
+            <div className="exchange__header--accounts flex">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
