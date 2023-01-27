622cefa677c6050778b1b8af8b0a2903168c1a58
Anton Laza <anton.laza.0116@gmail.com>
2023-01-27 23:38:00 +0100

    fetched balances from store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from '../assets/logo.png';
 
 const Navbar = () => {
     const account = useSelector(state => state.provider.account)
-
+    const balance = useSelector(state => state.provider.balance)
     return(
         <div className="exchange__header grid">
             <div className="exchange__header--brand flex">
@@ -17,7 +17,18 @@ const Navbar = () => {
             </div>
 
             <div className="exchange__header--account flex">
-                <a href=''>{account.slice(0,5) + '...' + account.slice(38,42)}</a>
+                {balance ? (
+                    <p><small>My Balance</small>{Number(balance).toFixed(4)}</p>
+                ) : (
+                    <p><small>My Balance</small>0 ETH</p>
+                )
+                }
+
+                {account ? (
+                    <a href=''>{account.slice(0,5) + '...' + account.slice(38,42)}</a> 
+                ) : (
+                    <a href=''/>
+                )}
             </div>
         </div>
     )
