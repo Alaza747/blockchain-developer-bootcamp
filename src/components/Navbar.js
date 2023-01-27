718a780db6f6f375652c11de758b7fb64c95ed87
Anton Laza <anton.laza.0116@gmail.com>
2023-01-27 23:23:45 +0100

    imported logo, fetched account address from redux

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,14 @@
+import { useSelector } from 'react-redux';
+
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
+    const account = useSelector(state => state.provider.account)
+
     return(
         <div className="exchange__header grid">
             <div className="exchange__header--brand flex">
+                <img src={logo} className="logo" alt='Tony Logo'></img>
                 <h1>Tony Token Exchange</h1>
             </div>
 
@@ -10,8 +16,8 @@ const Navbar = () => {
 
             </div>
 
-            <div className="exchange__header--accounts flex">
-                
+            <div className="exchange__header--account flex">
+                <a href=''>{account.slice(0,5) + '...' + account.slice(38,42)}</a>
             </div>
         </div>
     )
