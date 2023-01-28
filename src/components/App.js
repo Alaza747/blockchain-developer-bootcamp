78858091d0a3fc4bc45e7347a2689cae47cea8c9
Anton Laza <anton.laza.0116@gmail.com>
2023-01-28 15:48:08 +0100

    implemented marketSelector

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import {
 } from '../store/interactions';
 
 import Navbar from './Navbar';
+import Markets from './Markets';
 
 function App() {
   const dispatch = useDispatch();
@@ -48,12 +49,12 @@ function App() {
   return (
     <div>
 
-      <Navbar/>
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+          <Markets />
 
           {/* Balance */}
 
