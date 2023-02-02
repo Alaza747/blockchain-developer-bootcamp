968f2e1701908b66b2b619e413055c9f7d89d9b5
Anton Laza <anton.laza.0116@gmail.com>
2023-02-02 16:17:19 +0100

    defined makeSellOrder()

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -123,3 +123,21 @@ export const makeBuyOrder = async (provider, exchange, tokens, order, dispatch) 
     }
 
 }
+
+export const makeSellOrder = async (provider, exchange, tokens, order, dispatch) => {
+    const tokenGet = tokens[1].address
+    const amountGet = ethers.utils.parseEther((order.amount * order.price).toString(), 18)
+    const tokenGive = tokens[0].address
+    const amountGive = ethers.utils.parseEther(order.amount, 18)
+    
+    dispatch({ type: 'NEW_ORDER_REQUEST' })
+
+    try {    
+        const signer = await provider.getSigner()
+        const transaction = await exchange.connect(signer).makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+        await transaction.wait()
+    } catch (error) {
+        dispatch({ type: 'NEW_ORDER_FAIL' })
+    }
+
+}
