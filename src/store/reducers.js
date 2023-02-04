2ea8696dfd586dc1ab8268a29d869f7aa2eea7c6
Anton Laza <anton.laza.0116@gmail.com>
2023-02-04 13:15:45 +0100

    typo, cleaned code, comments

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -15,7 +15,7 @@ export const provider = (state = {}, action) => {
                 ...state,
                 account: action.account
             }
-        case 'BALANCE_LOADED':
+        case 'ETHER_BALANCE_LOADED':
             return {
                 ...state,
                 balance: action.balance
@@ -87,14 +87,20 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                 contract: action.exchange
             }
 
+        // ----------------------------------------------------------------
+        // ORDERS LOADED (CANCELLED, FILLED & ALL)
+
         case 'ALL_ORDERS_LOADED':
             return {
                 ...state,
                 allOrders: {
                     loaded: true,
-                    data: action.orders
+                    data: action.allOrders
                 }
             }
+
+        // ----------------------------------------------------------------
+        // BALANCE CASES
 
         case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
             return {
@@ -106,6 +112,10 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                 ...state,
                 balances: [...state.balances, action.balance]
             }
+
+        // ----------------------------------------------------------------
+        // TRANSFER CASES (DEPOSIT, WITHDRAWS)
+
         case 'TRANSFER_REQUEST':
             return {
                 ...state,
@@ -125,7 +135,7 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                     isSuccesful: true
                 },
                 transferInProgress: false,
-                events: [action.events, ...state.events]
+                events: [action.event, ...state.events]
             }
         case 'TRANSFER_FAIL':
             return {
@@ -137,8 +147,11 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                     isError: true
                 },
                 transferInProgress: false,
-                events: [action.events, ...state.events]
             }
+
+        // ----------------------------------------------------------------
+        // MAKING ORDERS CASES
+
         case 'NEW_ORDER_REQUEST':
             return {
                 ...state,
@@ -148,20 +161,11 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                     isSuccesful: false
                 }
             }
-        case 'NEW_ORDER_FAIL':
-            return {
-                ...state,
-                transaction: {
-                    transactionType: 'New Order',
-                    isPending: false,
-                    isSuccesful: false,
-                    isError: true
-                }
-            }
         case 'NEW_ORDER_SUCCESS':
             // Prevent duplicate orders
-            index = state.allOrders.data.findIndex(order => order.id === action.orderId)
-             
+            console.log(data)
+            index = state.allOrders.data.findIndex(order => order.id.toString() === action.order.id.toString())
+            
             if (index === -1) {
                 data = [...state.allOrders.data, action.order]
             } else {
@@ -182,6 +186,16 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
                 events: [action.event, ...state.events]
             }
 
+        case 'NEW_ORDER_FAIL':
+            return {
+                ...state,
+                transaction: {
+                    transactionType: 'New Order',
+                    isPending: false,
+                    isSuccesful: false,
+                    isError: true
+                }
+            }
         default:
             return state
     }
