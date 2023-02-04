ec6bed4b318433ef08b60983c4159af2cbe50738
Anton Laza <anton.laza.0116@gmail.com>
2023-02-04 19:10:13 +0100

    cases for cancelled & filled orders

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -90,6 +90,22 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
         // ----------------------------------------------------------------
         // ORDERS LOADED (CANCELLED, FILLED & ALL)
 
+        case 'CANCELLED_ORDERS_LOADED':
+            return {
+                ...state,
+                cancelledOrders: {
+                    loaded: true,
+                    data: action.cancelledOrders
+                }
+            }
+        case 'FILLED_ORDERS_LOADED':
+            return {
+                ...state,
+                filledOrders: {
+                    loaded: true,
+                    data: action.filledOrders
+                }
+            }
         case 'ALL_ORDERS_LOADED':
             return {
                 ...state,
