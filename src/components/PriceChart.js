6609649dfe7831381fc2a1c9e2ec4e91415ed7a7
Anton Laza <anton.laza.0116@gmail.com>
2023-02-05 14:07:07 +0100

    created priceChart and got series from Selector

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,38 +1,50 @@
 import { useSelector } from "react-redux";
 
+import Chart from "react-apexcharts";
+
+import { options, series } from "./PriceChart.config";
+
+import { priceChartSelector } from "../store/selectors";
+
 import Banner from "./Banner";
 
 const PriceChart = () => {
     const account = useSelector(state => state.provider.account);
+    const symbols = useSelector(state => state.tokens.symbols);
+    const priceChart = useSelector(priceChartSelector);
 
     return (
-      <div className="component exchange__chart">
-        <div className='component__header flex-between'>
-          <div className='flex'>
-  
-            <h2></h2>
-  
-            <div className='flex'>
-              {/* <img src="" alt="Arrow down" /> */}
-              <span className='up'></span>
+        <div className="component exchange__chart">
+            <div className='component__header flex-between'>
+                <div className='flex'>
+
+                    <h2>{symbols && `${symbols[0]}/${symbols[1]}`}</h2>
+
+                    <div className='flex'>
+                        {/* <img src="" alt="Arrow down" /> */}
+                        <span className='up'></span>
+                    </div>
+
+                </div>
             </div>
-  
-          </div>
+
+            {!account ? (
+                <Banner text={'Please connect with Metamask'} />
+            ) : (
+                <Chart 
+                    type="candlestick"
+                    options={options}
+                    series={priceChart ? priceChart.series : series}
+                    width="100%"
+                    height="100%"
+                />
+            )}
+
+
+
+
         </div>
-  
-        {/* Price chart goes here */}
-
-        {!account? (
-            <Banner text={'Please connect with Metamask'} />
-        ) : (
-            <p>Price Chart placeholder</p>
-        )}
-
-
-
-  
-      </div>
     );
-  }
-  
-  export default PriceChart;
+}
+
+export default PriceChart;
