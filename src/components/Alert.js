310e8bc8202ca31f898756e2f00cec0242b3bfe5
Anton Laza <anton.laza.0116@gmail.com>
2023-02-12 21:07:30 +0100

    get network from config.json

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,13 @@
 import { useSelector } from "react-redux";
 import { useRef, useEffect } from "react";
 import { myEventsSelector } from "../store/selectors";
-
+import config from '../config.json';
 
 const Alert = () => {
     const alertRef = useRef(null);
 
     const account = useSelector(state => state.provider.account)
-
+    const network = useSelector(state => state.provider.network)
     const isPending = useSelector(state => state.exchange.transaction.isPending);
     const isError = useSelector(state => state.exchange.transaction.isError);
     const events = useSelector(myEventsSelector);
@@ -26,13 +26,13 @@ const Alert = () => {
     return (
         <div>
             {isPending ? (
-                
+
                 <div className="alert alert--remove" onClick={removeHandler} ref={alertRef}>
                     <h1>Transaction Pending...</h1>
                 </div>
 
             ) : isError ? (
-                
+
                 <div className="alert alert--remove" onClick={removeHandler} ref={alertRef}>
                     <h1>Transaction Will Fail</h1>
                 </div>
@@ -42,7 +42,7 @@ const Alert = () => {
                 <div className="alert alert--remove" onClick={removeHandler} ref={alertRef}>
                     <h1>Transaction Successful</h1>
                     <a
-                        href=''
+                        href={config[network] ? `${config[network].explorerURL}/tx/${events[0].transactionHash}` : '#'}
                         target='_blank'
                         rel='noreferrer'
                     >
